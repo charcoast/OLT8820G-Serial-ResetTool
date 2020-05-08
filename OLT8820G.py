2658b64973c754d6dd0b2cd14dfe9f7067a7feac
Augusto Gil de Oliveira <augustogildeoliveira@gmail.com>
2020-05-08 09:04:49 -0300

    Melhoria no código

--- OLT8820G.py
+++ OLT8820G.py
@@ -1,39 +1,55 @@
 import serial
+ser = None
 
-def verifica_porta():
-    while True:
-        try:
-            porta = str(input('Informe a porta: '))
-            
-            porta_verificar = serial.Serial(porta)
-            porta_verificar.close()
-            
-            print('Porta válida\n')
-            return porta
-        except serial.SerialException:
-            print('Porta inválida\n')
-            continue
+def verifica_porta(porta): # Verifica se a porta digitada está ativa no SO
+    try:
+        porta_verificar = serial.Serial(porta)
+        porta_verificar.close()
+        print('Porta válida\n')
+        return porta
+
+    except serial.SerialException:
+        print('Porta inválida\n')
+        return None
         
+def main():
+    global ser
+    porta = None
+    while True: # Loop para que o usuário digite uma porta até que ela seja válida
+        input_porta = str(input('Informe a porta: '))
+        porta = verifica_porta(input_porta)
+        if porta:
+            break
+    ser = serial.Serial(porta,'115200') #Inicia a comunicação serial na velocidade 115200, padrão da OLT
+    ser.close() # Fecha qualquer conexão já existente na porta 
+    ser.open() # Inicia uma nova conexão na porta
+    print('Conectado à porta ' + porta)
+    aguarda_boot()
+    executa_comandos(b'cd datastor\n')
+    ser.close()
 
-porta = verifica_porta()
-ser = serial.Serial(porta,'115200')
-ser.close()
-ser.open()
-
-loop_boot_interrupt = True
-while True:
-    rec = ser.readline()
-    print(rec.decode('utf-8'))
-    boot_interrupt_msg = 'Prepare to load full image...'
-    for boot_interrupt_msg in rec.decode('utf-8'):
-        ser.write(b'zhone\n\n')
-        print('zhone')
-        loop_boot_interrupt = False
-        break
+def executa_comandos(comando):
+    retorno = []
+    ser.write(comando)
+    ser.readline()
+    
     
 
-ser.close()
+def aguarda_boot(): # Aguarda pela mensagem de boot e o interrompe
+    print('Aguardando o boot da OLT. \nReinicie-a para que o processo inicie.')
 
+    boot_interrupt_msg = 'Prepare to load full image...'
 
+    while True:
+        rec = ser.readline()
+        print(rec.decode('utf-8'))
+        
+        for boot_interrupt_msg in rec.decode('utf-8'):
+            ser.write(b'zhone\n\n')
+            print('zhone')
+            return None
+
+if __name__ == "__main__":
+    main()
     
     
